47859e606fcbd8bbde6e1538fcbb8d63cf6447f1
Jean Carlos <jeanxh4@gmail.com>
2024-02-11 16:32:34 -0300

    console.log deleted.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ chatHelp = document.querySelector('.chatHelp');
 function ajudar(){
     chat = document.querySelector('.talk-message');
     chat.classList.toggle('active');
-    console.log("jean")
 }
 
 chatHelp.addEventListener('click', ajudar);

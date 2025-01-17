57702cd534bad5ac623429a01688ea9f268badd3
Jean Carlos <jeanxh4@gmail.com>
2025-01-16 23:57:44 -0300

    .

---
+++ carrosel.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 18
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18
+      },
+      // when window width is >= 640px
+      1188: {
+        slidesPerView: 3,
+        spaceBetween: 28
+      }
+    },
+
+    /*
+    effect: 'flip',
+    flipEffect: {
+      slideShadows: false,
+    },
+    */
+  
+});
